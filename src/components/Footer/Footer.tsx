ab96d6d9504b0c470ba1be9b8ab6dabaaa0f4732
Nico Parra <no.gnzlz@gmail.com>
2024-07-02 16:55:29 -0300

    add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,32 @@
 import clsx from "clsx";
 import React from "react";
+import { Linkedin } from "@/components/Icons/Linkedin";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ weight: "400", subsets: ["latin"] });
 
 const BG_CLASSES = "bg-gradient-to-tr from-slate-700 to-blue-950";
 
 export const Footer = () => {
-  return <div className={clsx("h-20 w-full", BG_CLASSES)} />;
+  return (
+    <div className={clsx("h-20 w-full mt-6", BG_CLASSES)}>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-row justify-center">
+          <a
+            href="https://www.linkedin.com/in/nicolasgonzalezparra/"
+            rel="noreferrer noopener"
+            target="_blank"
+            className="transform scale-100 md:scale-75 hover:scale-100 transition-all duration-100"
+          >
+            <Linkedin className="w-6 h-6" />
+          </a>
+        </div>
+      </div>
+      <p
+        className={clsx("text-center text-gray-100 text-xs", nunito.className)}
+      >
+        © Nicolas González Parra 2024. All rights reserved.
+      </p>
+    </div>
+  );
 };
