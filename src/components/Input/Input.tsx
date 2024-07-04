da3b3342b643d35e77a94cd87bb2ec24bd3178be
Nico Parra <no.gnzlz@gmail.com>
2024-07-04 15:28:05 -0300

    add nunito to inputs

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useFormContext } from "react-hook-form";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"], weight: "300" });
 
 interface Props {
   className?: string;
@@ -29,7 +32,7 @@ export const Input = ({ className, name, type, placeholder }: Props) => {
         outline-1
          outline-slate-700 
          outline-opacity-50
-         outline-offset-2 ${className}`}
+         outline-offset-2 ${className} ${nunito.className}`}
     />
   );
 };
